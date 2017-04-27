f5852150d321bb710a1e65b527349b26afe54585
Justin Guyton <guyton.justin@gmail.com>
2017-04-27 02:25:57 -0500

    updated ui

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ app.debug = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['postgresql://postgres@localhost/raPlus']
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 # 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost/raPlus'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost/raPlus'
 
 #import modules after init app
 db = SQLAlchemy(app)
